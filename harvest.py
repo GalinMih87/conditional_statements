a7a2b97be253a0d0357834ae7a8b402680dec8ae
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-12 10:47:11 +0300

    Update harvest.py

--- harvest.py
+++ harvest.py
@@ -3,7 +3,7 @@ from math import floor, ceil
 vineyard = int(input())
 grapes = float(input())
 liters_needed = int(input())
-robi = int(input())
+people = int(input())
 
 obshto_grozde = vineyard * grapes
 vino = 0.4 * (obshto_grozde / 2.5)
@@ -14,4 +14,4 @@ if vino < liters_needed:
     print(f"It will be a tough winter! More {floor(diff)} liters wine needed.")
 elif vino >= liters_needed:
     print(f"Good harvest this year! Total wine: {floor(vino)} liters.")
-    print(f"{ceil(diff)} liters left -> {ceil(diff / robi)} liters per person.")
+    print(f"{ceil(diff)} liters left -> {ceil(diff / people)} liters per person.")
