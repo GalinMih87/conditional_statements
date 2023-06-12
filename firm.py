3008bfa5a0df30bce882fdbac998dff4e6b6ad71
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-12 10:41:10 +0300

    Update firm.py

--- firm.py
+++ firm.py
@@ -5,8 +5,8 @@ d = int(input())
 peoples = int(input())
 
 training = (d - (d * 0.1)) * 8
-izvanreden_trud = peoples * (2 * d)
-obshto_chasove = floor(training + izvanreden_trud)
+overtime = peoples * (2 * d)
+obshto_chasove = floor(training + overtime)
 
 diff = abs(obshto_chasove - h)
 if obshto_chasove >= h:
