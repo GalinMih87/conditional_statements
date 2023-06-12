b67f3cd2387f248799242104b21a0645b850038f
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-12 10:55:08 +0300

    Update pipes_in_pool.py

--- pipes_in_pool.py
+++ pipes_in_pool.py
@@ -8,10 +8,10 @@ p2_l = p2 * h
 total = p1_l + p2_l
 total_procent = (total / v) * 100
 
-prinos_p1 = (p1_l / total) * 100
-prinos_p2 = (p2_l / total) * 100
+contribution_p1 = (p1_l / total) * 100
+contribution_p2 = (p2_l / total) * 100
 
 if v >= total:
-    print(f"The pool is {total_procent:.2f}% full. Pipe 1: {prinos_p1:.2f}%. Pipe 2: {prinos_p2:.2f}%.")
+    print(f"The pool is {total_procent:.2f}% full. Pipe 1: {contribution_p1:.2f}%. Pipe 2: {contribution_p2:.2f}%.")
 else:
     print(f"For {h:.2f} hours the pool overflows with {total - v:.2f} liters.")
