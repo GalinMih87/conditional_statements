8285a2c7a1ecea86916949694e963107662259b9
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-19 11:18:57 +0300

    Добавих !!!
    Добавих !!! Добавих 3 ! знака, защото бяха изпуснати.

--- sleepy_tom_cat.py
+++ sleepy_tom_cat.py
@@ -1,16 +1,16 @@
-pochivni_dni = int(input()) # Да го променя на английски език
-norma_za_igra = 30000 # Да го променя на английски език
+pochivni_dni = int(input()) # Да го променя на английски език!!!
+norma_za_igra = 30000 # Да го променя на английски език!!!
 
-pochivni_dni_minuti = pochivni_dni * 127 # Да го променя на английски език
-rabotni_dni_minuti = (365 - pochivni_dni) * 63 # Да го променя на английски език
-obshto_minuti = pochivni_dni_minuti + rabotni_dni_minuti # Да го променя на английски език
+pochivni_dni_minuti = pochivni_dni * 127 # Да го променя на английски език!!!
+rabotni_dni_minuti = (365 - pochivni_dni) * 63 # Да го променя на английски език!!!
+obshto_minuti = pochivni_dni_minuti + rabotni_dni_minuti # Да го променя на английски език!!!
 
-diff = abs(norma_za_igra - obshto_minuti) # Да го променя на английски език
+diff = abs(norma_za_igra - obshto_minuti) # Да го променя на английски език!!!
 
 h = diff // 60
 m = diff % 60
 
-if norma_za_igra >= obshto_minuti: # Да го променя на английски език
+if norma_za_igra >= obshto_minuti: # Да го променя на английски език!!!
     print(f"Tom sleeps well")
     print(f"{h} hours and {m} minutes less for play")
 elif norma_za_igra <= obshto_minuti:
