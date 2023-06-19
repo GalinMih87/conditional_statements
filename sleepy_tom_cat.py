98744f705b3324e2728982bfac2c6f7e9b04e3c9
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-19 11:32:14 +0300

    Update sleepy_tom_cat.py

--- sleepy_tom_cat.py
+++ sleepy_tom_cat.py
@@ -3,16 +3,16 @@ norm_of_play = 30000 # Да го променя на английски език
 
 holidays_in_minutes = holidays * 127 # Да го променя на английски език!!!
 working_days_in_minutes = (365 - holidays) * 63 # Да го променя на английски език!!!
-obshto_minuti = holidays_in_minutes + working_days_in_minutes  # Да го променя на английски език!!!
+total_minutes = holidays_in_minutes + working_days_in_minutes  # Да го променя на английски език!!!
 
-diff = abs(norm_of_play - obshto_minuti) # Да го променя на английски език!!!
+diff = abs(norm_of_play - total_minutes) # Да го променя на английски език!!!
 
 h = diff // 60
 m = diff % 60
 
-if norm_of_play >= obshto_minuti: # Да го променя на английски език!!!
+if norm_of_play >= total_minutes: # Да го променя на английски език!!!
     print(f"Tom sleeps well")
     print(f"{h} hours and {m} minutes less for play")
-elif norm_of_play <= obshto_minuti:
+elif norm_of_play <= total_minutes:
     print(f"Tom will run away")
     print(f"{h} hours and {m} minutes more for play")
